68128927065b0f249201755b0c1dd60ccc5a2f51
lordtvlor <lordtvlor@gmail.com>
2026-04-07 21:05:17 +1200

    Feature: reading from config file
    Completed implementing the ability for a daemon class to write itself from a config file

--- Assign1/routing_daemon.py
+++ Assign1/routing_daemon.py
@@ -19,34 +19,50 @@ def removeComments(lines):
             importantLines.append(line)
     return importantLines
 
+def rangeCheck(value, min, max, varName):
+    if value < min or value > max:
+        raise  ValueError(f"{varName}: {value} must be in range {min} to {max}")
+
 def extractData(lines):
     data = {}
     idLine = lines[0].split()
-    inportLine = lines[1].split()
-    outportLine = lines[2].split()
-    if len(lines) > 3 :
-        argsLine = lines[3].split()
-
     if len(idLine) > 2:
         raise IndexError("There are too many indices for this field.")
     elif idLine[0] != "id":
         raise FieldNotFoundError("id")
     else:
-        if int(idLine[1]) < 1 or int(idLine[1]) > 64000:
-            raise ValueError("id must be in range 1-64000.")
-        else:
-            data['id'] = int(idLine[1])
+        rangeCheck(idLine[1], 1, 64000, "ID")
+        data['id'] = int(idLine[1])
 
+
+    inportLine = lines[1].split()
     inports = []
     if inportLine[0] != "inport":
         raise FieldNotFoundError("inport")
     else:
         for inport in inportLine[1:]:
-            if int(inport) < 1024 or int(inport) > 64000:
-                raise ValueError(f"inport {inport} not in range 1024-64000.")
-            else:
-                inports.append(int(inport))
+            rangeCheck(inport, 1024, 64000, "Inport")
+            inports.append(int(inport))
     data['inports'] = inports
+    outportLine = lines[2].split()
+    if outportLine[0] != "outport":
+        raise FieldNotFoundError("outport")
+    else:
+        for outport in outportLine[1:]:
+            outport = outport[1:-1]
+            portnum, cost, otherId = outport.split(',')
+            rangeCheck(int(portnum), 1024, 64000, "Outport portnum")
+            rangeCheck(int(cost), 0, float('inf'), "Cost")
+            rangeCheck(int(otherId), 1, 64000, "Other ID")
+            data['outports'] = (int(portnum), int(cost), int(otherId))
+
+    if len(lines) > 3 :
+        argsLine = lines[3].split()
+        data['args'] = [int(argsLine[1])]
+        for arg in argsLine[2:]:
+            data['args'].append(arg)
+
+    return data
 
 class RoutingDaemon:
     def __init__(self):
@@ -55,6 +71,9 @@ class RoutingDaemon:
         contents = removeComments(contents)
         data = extractData(contents)
         self.id = data['id']
+        self.inports = data['inports']
+        self.outports = data['outports']
+        self.args = data['args']
 
 if __name__ == '__main__':
     daemon = RoutingDaemon()
